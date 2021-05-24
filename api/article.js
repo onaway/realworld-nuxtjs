379ba43c09dba5347203df8e477fe0eb0502ecff
zhengxiaoqing <zhengxiaoqing@enjoymi.com>
2021-05-24 18:22:41 +0800

    增加评论，获取、发表、更新、删除文章接口

--- api/article.js
+++ api/article.js
@@ -1,7 +1,7 @@
 import { request } from '@/plugins/request';
 
-// 文章列表
-export const getArticles = params => {
+// 所有文章列表
+export const getAllArticles = params => {
     return request({
         method: 'GET',
         url: '/api/articles',
@@ -39,5 +39,56 @@ export const getArticleDetail = slug => {
     return request({
         method: 'GET',
         url: `/api/articles/${slug}`
+    })
+}
+
+// 评论列表
+export const getComments = slug => {
+    return request({
+        method: 'GET',
+        url: `/api/articles/${slug}/comments`
+    })
+}
+
+// 发表评论
+export const addComments = (data, slug) => {
+    return request({
+        method: 'POST',
+        url: `/api/articles/${slug}/comments`,
+        data
+    })
+}
+
+// 获取文章
+export const getArticle = slug => {
+    return request({
+        method: 'GET',
+        url: `/api/articles/${slug}`
+    })
+}
+
+// 发表文章
+export const createArticle = data => {
+    return request({
+        method: 'POST',
+        url: '/api/articles',
+        data
+    })
+}
+
+// 更新文章
+export const updateArticle = (data, slug) => {
+    return request({
+        method: 'PUT',
+        url: `/api/articles/${slug}`,
+        data
+    })
+}
+
+// 删除文章
+export const delArticle = slug => {
+    return request({
+        method: 'DELETE',
+        url: `/api/articles/${slug}`,
     })
 }
