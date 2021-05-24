7d40f355894e2868d51851fb061b23632ba88a3d
zhengxiaoqing <zhengxiaoqing@enjoymi.com>
2021-05-24 18:25:19 +0800

    自己和他人的显示更改及功能完善

--- pages/article/components/article-meta.vue
+++ pages/article/components/article-meta.vue
@@ -8,20 +8,37 @@
                 {{ article.author.username }}</nuxt-link>
             <span class="date">{{ article.createdAt | date('MMM DD, YYYY') }}</span>
         </div>
-        <button class="btn btn-sm btn-outline-secondary" :class="{ active: article.author.following }">
-            <i class="ion-plus-round"></i>
-            Follow {{ article.author.username }}
-        </button>
-        &nbsp;&nbsp;
-        <button class="btn btn-sm btn-outline-primary" :class="{ active: article.favorited }">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Article <span class="counter">({{ article.favoritesCount }})</span>
-        </button>
+
+        <!-- 自己的文章 -->
+        <template v-if="isYourself">
+            <nuxt-link class="btn btn-outline-secondary btn-sm" :to="{ name: 'Editor', params: { slug: article.slug } }">
+                <i class="ion-edit"></i> Edit Article
+            </nuxt-link>
+            &nbsp;&nbsp;
+            <button class="btn btn-outline-danger btn-sm" @click="deleteArticle(article.slug)">
+                <i class="ion-trash-a"></i> Delete Article
+            </button>
+        </template>
+
+        <!-- 他人的文章 -->
+        <template v-else>
+            <button class="btn btn-sm btn-outline-secondary" :class="{ active: article.author.following }">
+                <i class="ion-plus-round"></i>
+                Follow {{ article.author.username }}
+            </button>
+            &nbsp;&nbsp;
+            <button class="btn btn-sm btn-outline-primary" :class="{ active: article.favorited }">
+                <i class="ion-heart"></i>
+                &nbsp;
+                Favorite Article <span class="counter">({{ article.favoritesCount }})</span>
+            </button>
+        </template>
     </div>
 </template>
 
 <script>
+import { delArticle } from '@/api/article'
+
 export default {
     name: "ArticleMeta",
     props: {
@@ -29,11 +46,25 @@ export default {
             type: Object,
             require: true,
         },
+        user: {
+            type: Object,
+            require: true
+        }
     },
     data() {
         return {};
     },
-    methods: {},
+    computed: {
+        isYourself() {
+            return this.article.author.username === this.user.username
+        }
+    },
+    methods: {
+        async deleteArticle(slug) {
+            await delArticle(slug)
+            this.$router.push({ name: 'Home' })
+        }
+    },
 };
 </script>
 
